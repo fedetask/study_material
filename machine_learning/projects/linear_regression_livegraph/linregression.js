5a4cfefb49c458d20cc6fd9b6c5f58da9dd53c95
Federico Taschin <fedetask@gmail.com>
2018-12-16 18:33:12 +0100

    Animation, result log, input values
    - User can set learnng rate and stop method (iterations or improvement)
    - Results and steps are logged in a textarea
    - Animation works

--- machine_learning/projects/linear_regression_livegraph/linregression.js
+++ machine_learning/projects/linear_regression_livegraph/linregression.js
@@ -4,7 +4,11 @@ let scatter = {
   x: [3, 4, 5, 6, 8, 10,],
   y: [3, 5, 3.5, 7, 9.2, 9],
   mode: 'markers',
-  type: 'scatter'
+  type: 'scatter',
+  marker: {
+    size: 10
+  },
+  name: 'Points'
 }
 
 let emptyLine = {
@@ -13,9 +17,16 @@ let emptyLine = {
   mode: 'lines'
 }
 
-result = gradientDescent(scatter.x, scatter.y, false)
+var improvement
+var learningRate
+var maxIterations
+
+
+readInput()
+result = gradientDescent(scatter.x, scatter.y, false, false)
 currLine = getLine(result.m, result.q)
 createGraph(scatter, currLine)
+printlog(pointsAsString())
 
 /* Our cost function is given by the sum of the squared errors:
  * for each (x,y), we calculate the hypothesis h(x) and the error h(x) - y
@@ -66,7 +77,7 @@ function derivatives(pointsx, pointsy, m, q) {
  * @param {q}       current q value of the hypothesys
  * @return          array {newm: new value of m, newq: new value of q}
  */
-function update(pointsx, pointsy, m, q, learningRate) {
+function update(pointsx, pointsy, m, q) {
   ders = derivatives(pointsx, pointsy, m, q)
   let newm = m - learningRate * ders.dm
   let newq = q - learningRate * ders.dq
@@ -80,34 +91,52 @@ function update(pointsx, pointsy, m, q, learningRate) {
  * @param learningRate learning rate parameter that the derivative is multiplied for
  * @param iterations   number of iterations to do
  */
-function gradientDescent(pointsx, pointsy, animate) {
+function gradientDescent(pointsx, pointsy, animate, log) {
   let m = 0;
   let q = 0;
-  let learningRate = 0.5
   let numIterations = 0
-  while (true) {
+  clearLog()
+  if (log) {
+	  printlog(pointsAsString())
+	  printlog('Starting gradient descent: m = 0, q = 0')
+  }
+  
+	while (true) {
 	  if(animate){
 		animateLine(m,q)
 	  }
 	  let prevCost = cost(pointsx, pointsy, m, q) //Cost for the current values of m and q
-	  let updated = update(pointsx, pointsy, m, q, learningRate) // Calculating new values for m and q
+	  let updated = update(pointsx, pointsy, m, q) // Calculating new values for m and q
 	  let newCost = cost(pointsx, pointsy, updated.newm, updated.newq) // Cost after update
 	  
 	  numIterations ++
 	  
 	  while(newCost > prevCost) { // If the cost doesn't decrease, we reduce the learning rate
+		  printlog('LEARNING RATE TOO HIGH: COST IS INCREASING!')
 		  learningRate = learningRate/2
-		  updated = update(pointsx, pointsy, m, q, learningRate)
+		  printlog('LEARNING RATE DECREASED: '+learningRate)
+		  setLearningRate(''+learningRate)
+		  updated = update(pointsx, pointsy, m, q)
 		  newCost = cost(pointsx, pointsy, updated.newm, updated.newq) // Until new cost is lower
 	  }
 	  
 	  m = updated.newm
-	  q = updated.newq
+	  q = updated.newq	  
 	  currLine = getLine(m, q)
+	  let improv = Math.abs(newCost - prevCost)
+	  if(log)printlog('Iteration '+numIterations+' m = '+m.toFixed(3)+', q = '+q.toFixed(3)+', cost decreased by '+improv.toFixed(8))
 	  
-	  let improvement = Math.abs(newCost - prevCost)
-	  if (improvement < 0.0001) break
+	  if (improvement && improv <improvement) {
+		  console.log('Program finished because of improvement')
+		  break
+	  }
+
+	  if (maxIterations && numIterations === maxIterations) {
+		  console.log('Program finished because of iterations')
+		  break
+	  }
   }
+  if(log)printlog('Finished gradient descent: m = '+m+'   q = '+q)
   return { m: m, q: q, iterations: numIterations}
 }
 
@@ -120,35 +149,31 @@ function pointExists(x, y) {
 }
 
 function addPoint() {
-  let xstr = document.getElementById('pointx').value
-  let ystr = document.getElementById('pointy').value
-  if(!xstr || !ystr) {
-	  console.log('Empty input')
-	  return
-  }
-  let newx = Number(xstr)
-  let newy = Number(ystr)
+	readInput()
+	let xstr = document.getElementById('pointx').value
+	let ystr = document.getElementById('pointy').value
+	if(!xstr || !ystr) {
+		console.log('Empty input')
+		return
+	}
+	let newx = Number(xstr)
+	let newy = Number(ystr)
   
-  if (pointExists(newx, newy)) {
-	  console.log('Already exists '+newx+'  '+newy)
-	  return
-  }
+	if (pointExists(newx, newy)) {
+		console.log('Already exists '+newx+'  '+newy)
+		return
+	}
+	
+	scatter.x.push(newx)
+	scatter.y.push(newy)
   
-  scatter.x.push(newx)
-  scatter.y.push(newy)
-  
-  createGraph(scatter, currLine)
-  
-  //if (scatter.x <= 1) {
-//	  createGraph()
-//	  return 
- // }
-  
-  let result = gradientDescent(scatter.x, scatter.y, true)
-  console.log('Gradient descent stopped after '+result.iterations+' iterations.   m: '+result.m+'  q: '+result.q)
-  
-  let m = result.m
-  let q = result.q
+	let result = gradientDescent(scatter.x, scatter.y, false, true)
+	console.log('Gradient descent stopped after '+result.iterations+' iterations.   m: '+result.m+'  q: '+result.q)
+	
+	let m = result.m
+	let q = result.q
+	
+	createGraph(scatter, currLine)
   }
 
 function getLine(m, q) {
@@ -158,7 +183,8 @@ function getLine(m, q) {
   let line = {
   x: [],
   y: [],
-  mode: 'lines'
+  mode: 'lines',
+  name: 'Fitting'
   }
 
   range = (max - min) ? (max - min) : 1
@@ -195,7 +221,9 @@ function createGraph(scatter, line) {
 }
 
 function runAnimation(){
-	gradientDescent(scatter.x, scatter.y, true)
+	readInput()
+	createGraph(scatter, getLine(0, 0))
+	gradientDescent(scatter.x, scatter.y, true, true)
 }
 
 function animateLine(m, q) {
@@ -208,7 +236,7 @@ function animateLine(m, q) {
 			},
 			{
 				transition: {
-				duration: 1000
+				duration: 1000	
 			},
 				frame: {
 					duration: 1000,
@@ -216,4 +244,63 @@ function animateLine(m, q) {
 				}
 			}
 		  )
+}
+
+function readInput() {
+	learningRate = Number(document.getElementById('learningRate').value)
+	iterationsRadio = document.getElementById('iterationsRadio')
+	improvementRadio = document.getElementById('improvementRadio')
+	iterationsInput = document.getElementById('iterationsNumber')
+	improvementInput = document.getElementById('improvementValue')
+	
+	if(iterationsRadio.checked) {
+		maxIterations = Number(iterationsInput.value)
+		improvement = undefined
+	} else {
+		improvement = Number(improvementInput.value)
+		maxIterations = undefined
+	}
+}
+
+function recompute() {
+	result = gradientDescent(scatter.x, scatter.y, false, true)
+	currLine = getLine(result.m, result.q)
+	createGraph(scatter, currLine)
+}
+
+function removePointButton() {
+	let x = Number(document.getElementById('pointx').value)
+	let y = Number(document.getElementById('pointy').value)
+	removePoint(x,y)
+	recompute()
+}
+
+function removePoint(x, y) {
+	for (let i = 0; i < scatter.x.length; i++) {
+		if (scatter.x[i] === x && scatter.y[i] === y) {
+			scatter.x.splice(i, 1)
+			scatter.y.splice(i, 1)
+			recompute()
+		}
+	}
+}
+
+function printlog(val) {
+	document.getElementById('resultlog').value += val+'\n'
+}
+
+function clearLog() {
+	document.getElementById('resultlog').value = '\n'	
+}
+
+function setLearningRate(rate) {
+	document.getElementById('learningRate').value = rate
+}
+
+function pointsAsString() {
+	str = 'Points: '
+	for (let i = 0; i < scatter.x.length; i++) {
+		str += '('+scatter.x[i]+', '+scatter.y[i]+') '
+	}
+	return str
 }
